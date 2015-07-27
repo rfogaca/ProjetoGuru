077cd63903942ee0955532400422942b47490dbe
rfogaca <rodrigofogacars@gmail.com>
2015-07-27 17:36:05 -0300

    Ajustes na tela de respostas.

--- ProjetoGuru/ProjetoGuru/Controllers/RespostaController.cs
+++ ProjetoGuru/ProjetoGuru/Controllers/RespostaController.cs
@@ -54,8 +54,8 @@ namespace ProjetoGuru.Controllers
         {
             if (ModelState.IsValid)
             {
-                db.Pergunta.SqlQuery("UPDATE Pergunta SET Status='R' WHERE PerguntaID="+resposta.PerguntaID);
-                
+                var pergunta = db.Pergunta.SqlQuery("UPDATE Pergunta SET Status='R' WHERE PerguntaID="+resposta.PerguntaID);
+
                 resposta.Data = DateTime.Now;
                 db.Resposta.Add(resposta);
                 db.SaveChanges();
