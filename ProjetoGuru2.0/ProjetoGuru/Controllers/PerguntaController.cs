ab0c31802595994a84d02035b467228333e180b5
Felipe Vencato <felipe.vencato@rcadigital.com.br>
2015-08-06 18:52:08 -0300

    Adicionando controllers e removento view não usadas

---
+++ ProjetoGuru2.0/ProjetoGuru/Controllers/PerguntaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoGuru.Controllers
+{
+    public class PerguntaController : Controller
+    {
+        // GET: Pergunta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ProjetoGuru2.0/ProjetoGuru/Controllers/RespostaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjetoGuru.Controllers
+{
+    public class RespostaController : Controller
+    {
+        // GET: Resposta
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
