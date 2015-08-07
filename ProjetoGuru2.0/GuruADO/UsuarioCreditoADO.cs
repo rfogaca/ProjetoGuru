c92797806344f0ae37231f8ae0802e40d7c74af4
Felipe Vencato <felipe.vencato@rcadigital.com.br>
2015-08-07 15:12:28 -0300

    Adicionando TipoUsuarioADO e CreditoUsuarioADO

--- ProjetoGuru2.0/GuruADO/UsuarioCreditoADO.cs
+++ ProjetoGuru2.0/GuruADO/UsuarioCreditoADO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuruDataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,59 @@ namespace GuruADO
 	public class UsuarioCreditoADO : BaseADO
 	{
 		public UsuarioCreditoADO() : base() { }
+		//Lista todos os creditos TODOS
+		public List<UsuarioCredito> ListarUsuarioCreditos ()
+		{
+			return db.UsuarioCredito.ToList();
+		}
+		//Lista todos os creditos de um usuário
+		public List<UsuarioCredito> ConsultarUsuarioCreditos(int id)
+		{
+			return db.UsuarioCredito.Where(credito => credito.UsuarioID == id).OrderBy(credito => credito.Data).ToList();
+		}
+		public Boolean CreateUsuarioCredito(UsuarioCredito credito)
+		{
+			try
+			{
+				db.UsuarioCredito.Add(credito);
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean UpdateUsuarioCredito(UsuarioCredito credito)
+		{
+			try
+			{
+				UsuarioCredito updating = db.UsuarioCredito.Find(credito.UsuarioCreditoID);
+				updating.UsuarioCreditoID = credito.UsuarioCreditoID;
+				updating.UsuarioID = credito.UsuarioID;
+				updating.Usuario = credito.Usuario;
+				updating.Data = credito.Data;
+				updating.Valor = credito.Valor;
+				db.SaveChanges(); 
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean DeleteUsuarioCredito(int id)
+		{
+			try
+			{
+				db.UsuarioCredito.Remove(db.UsuarioCredito.Find(id));
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 	}
 }
--- ProjetoGuru2.0/GuruADO/UsuarioTipoADO.cs
+++ ProjetoGuru2.0/GuruADO/UsuarioTipoADO.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GuruDataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,55 @@ namespace GuruADO
 	public class UsuarioTipoADO : BaseADO
 	{
 		public UsuarioTipoADO() : base() { }
+		//Lista todos os tipos de usuario
+		public List<UsuarioTipo> ListarUsuarioTipos()
+		{
+			return db.UsuarioTipo.ToList();
+		}
+		public UsuarioTipo ConsultarUsuarioTipo(int id)
+		{
+			return db.UsuarioTipo.Where(tipo => tipo.UsuarioTipoID == id).FirstOrDefault();
+		}
+		public Boolean CreateUsuarioTipo(UsuarioTipo tipo)
+		{
+			try
+			{
+				db.UsuarioTipo.Add(tipo);
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean UpdateUsuarioTipo(UsuarioTipo tipo)
+		{
+			try
+			{
+				UsuarioTipo updating = db.UsuarioTipo.Find(tipo.UsuarioTipoID);
+				updating.UsuarioTipoID = tipo.UsuarioTipoID;
+				updating.NomeTipo = tipo.NomeTipo;
+				db.SaveChanges(); 
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean DeleteCategoria(int id)
+		{
+			try
+			{
+				db.UsuarioTipo.Remove(db.UsuarioTipo.Find(id));
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
 	}
 }
