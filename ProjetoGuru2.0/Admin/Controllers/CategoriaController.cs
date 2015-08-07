caaefb35b0699d73dde07175f08cec5e5bcc0214
Felipe Vencato <felipe.vencato@rcadigital.com.br>
2015-08-07 16:09:14 -0300

    Adicionado controles vasios para cada entidade. Ajuste inicial para cada controle. Código inicial do controle para Categoria

---
+++ ProjetoGuru2.0/Admin/Controllers/CategoriaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GuruDataModel;
+using GuruBO;
+
+namespace Admin.Controllers
+{
+    public class CategoriaController : Controller
+    {	
+		CategoriaBO bo = new CategoriaBO();
+        // GET: Categoria
+        public ActionResult Index()
+        {
+			return View();
+        }
+    }
+}
---
+++ ProjetoGuru2.0/Admin/Controllers/UsuarioTipoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Admin.Controllers
+{
+    public class UsuarioTipoController : Controller
+    {
+        // GET: UsuarioTipo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
