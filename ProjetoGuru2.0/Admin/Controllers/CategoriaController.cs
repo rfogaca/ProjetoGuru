512c53e0e71b63d6cd1925c9661a641f173280a9
Felipe Vencato <felipe.vencato@rcadigital.com.br>
2015-08-07 19:09:36 -0300

    Adição da parte inicial do controle de Categoria. Adição de pastas e configuração inicial das areas.

--- ProjetoGuru2.0/Admin/Controllers/CategoriaController.cs
+++ ProjetoGuru2.0/Admin/Controllers/CategoriaController.cs
@@ -14,6 +14,7 @@ namespace Admin.Controllers
         // GET: Categoria
         public ActionResult Index()
         {
+			
 			return View();
         }
     }
---
+++ ProjetoGuru2.0/Admin/Models/CategoriaViewModels.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Admin.Models
+{
+	public class CategoriaViewModels
+	{
+	}
+	public class ListarCategoriaViewModel
+	{
+		public int CategoriaID {get; set;}
+		public int CategoriaParentID { get; set; }
+		public string NomeCategoria { get; set; }
+	}
+	public class CreateCategoriaViewModel
+	{
+		public int CategoriaParent { get; set; }
+	}
+}
