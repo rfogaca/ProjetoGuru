c46a9439ca52e4b217d85f1a10046117f2cce139
Felipe Vencato <felipe.vencato@rcadigital.com.br>
2015-08-07 14:46:01 -0300

    Adicionando Pergunta ADO

--- ProjetoGuru2.0/GuruADO/PerguntaADO.cs
+++ ProjetoGuru2.0/GuruADO/PerguntaADO.cs
@@ -14,13 +14,66 @@ namespace GuruADO
 		//Lista todas as perguntas inclusive as deletadas
 		public List<Pergunta> ListarPerguntas(int usuarioID)
 		{
-			return db.Pergunta.Include(x => x.Resposta).ToList();
+			return db.Pergunta.Include(pergunta => pergunta.Resposta).ToList();
 		}
 		//Pesquisa pelas peguntas do susuários não deletadas
 		public List<Pergunta> ConsultaPerguntasUsuario (int usuarioID)
 		{
-			//return db.Pergunta.Where(pergunta => pergunta.PerguntaID == usuarioID && status != "D").OrderBy(x => x.Data).ToList();
+			return db.Pergunta.Where(pergunta => pergunta.PerguntaID == usuarioID && pergunta.Status != "D").OrderBy(pergunta => pergunta.Data).ToList();
 		}
 		//Pesquisa pelas perguntas do usuário de acordo com o status
+		public List<Pergunta> ConsultaPerguntasUsuario(int usuarioID, string status)
+		{
+			return db.Pergunta.Where(pergunta => pergunta.PerguntaID == usuarioID && pergunta.Status == status).OrderBy(pergunta => pergunta.Data).ToList();
+		}
+		public Boolean CreatePergunta(Pergunta pergunta)
+		{
+			try
+			{
+				db.Pergunta.Add(pergunta);
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean UpdatePergunta(Pergunta pergunta)
+		{
+			try
+			{
+				Pergunta updating = db.Pergunta.Find(pergunta.PerguntaID);
+				
+				updating.PerguntaID = pergunta.PerguntaID;
+				updating.UsuarioID = pergunta.UsuarioID;
+				updating.Usuario = pergunta.Usuario;
+				updating.CategoriaID = pergunta.CategoriaID;
+				updating.Categoria = pergunta.Categoria;
+				updating.Data = pergunta.Data;
+				updating.Imagem = pergunta.Imagem;
+				updating.Status = pergunta.Status;
+				updating.Texto = pergunta.Texto;
+				updating.Resposta = pergunta.Resposta;
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+		public Boolean DeletePergunta(int id)
+		{
+			try
+			{
+				db.Pergunta.Remove(db.Pergunta.Find(id));
+				db.SaveChanges();
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
     }
 }
